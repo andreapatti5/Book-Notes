d60332f6834ee3f02f3bc9dd7c1835e2ef87813f
apatti5 <andrea.patti5@yahoo.it>
2025-08-29 17:41:41 +0200

    secondo commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+const standardURL = "https://covers.openlibrary.org/b/isbn/";
+const searchURL = "https://openlibrary.org/search.json";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -17,8 +20,20 @@ const db = new pg.Client({
 });
 db.connect();
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT title, author, code FROM books order by id ASC");
+  const bookTitle = result.rows[0].title;
+  const bookAuthor = result.rows[0].author;
+  const bookCode = result.rows[0].code;
+  const imageURL = `${standardURL}${bookCode}-M.jpg`;
+
+if (bookCode) {
+  imageURL = `${URL}${bookCode}-M.jpg`;
+} else if (bookCode === null) {
+  await axios.get(`${searchURL}`)
+}
+
+    res.render("index.ejs", { title : bookTitle, author : bookAuthor, code : bookCode, image : imageURL });
 });
 
 app.listen(port, () => {
